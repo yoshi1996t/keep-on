ab9fbc8d1e42b70ed661a479bdba2f5bfcba4817
yoshi1996t <am.k.fs.sea0702@gmail.com>
2020-10-13 18:27:17 +0900

    ユーザーの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all
+    @users = User.all
   end
 
   def show
@@ -9,3 +9,7 @@ class UsersController < ApplicationController
   def edit
   end
 end
+
+private
+
+def 
